2b1608051bc3a9f579e062639255d832f1eda78f
user <user@smsng>
2018-09-07 11:30:12 +0500

    comment

--- src/autostart_manager/Main.java
+++ src/autostart_manager/Main.java
@@ -21,6 +21,7 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        //DEV BRANCH
         //getAutostartList();
 
         //TEST MENU REWRITE AFTER        
