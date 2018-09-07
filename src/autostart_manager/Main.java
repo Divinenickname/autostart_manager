ce0503de2abb401b39757af65c10b0527486071a
user <user@smsng>
2018-09-07 12:06:10 +0500

    Add menu

--- src/autostart_manager/Main.java
+++ src/autostart_manager/Main.java
@@ -6,10 +6,7 @@
 package autostart_manager;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Scanner;
+
 
 /**
  *
@@ -17,7 +14,6 @@ import java.util.Scanner;
  */
 
 public class Main {
-    enum menu {add, del, exit, list};
 
     public static void main(String[] args) {
         // TODO code application logic here
@@ -26,23 +22,25 @@ public class Main {
 
         //TEST MENU REWRITE AFTER        
         String cmd="";
-        BufferedReader bufferedReader = new BufferedReader(
-                new InputStreamReader(System.in));
-        while(!cmd.equals("exit")){
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        //Menu menu = new Menu(bufferedReader);
+
+        /*while(!cmd.equals("exit")){
             try{
             cmd = bufferedReader.readLine();
-            
+
             switch (cmd){
                 case "list":
-                    getAutostartList();
+                    Manager.getAutostartList();
                     break;
 
                 case "del":
-                    deleteApp();
+                    Manager.deleteApp();
                     break;
 
                 case "add":
-                    addApp();
+                    Manager.addApp();
                     break;
                     
                 case "help":
@@ -53,99 +51,6 @@ public class Main {
             catch(IOException e){
                 System.out.println(e);
             }
-        }
-    }
-    
-    private static String getHome(){
-        String homedir = System.getProperty("user.home")+"/.config/autostart";
-        return homedir;
-    }
-
-    private static String getAppPath(String appName){
-        String path;
-        path = getHome()+ "/" + appName+ ".desktop";
-        return path;
-    }
-    
-    public static void getAutostartList(){
-        String[] list;
-        File dir = new File(System.getProperty(
-                "user.home") + "/.config/autostart");
-        if(dir.isDirectory()){
-            list = dir.list();
-            for(String s: list){
-                System.out.println(s);
-            }
-        }
-        else{
-            System.out.println("Can't find directory");
-        }
-    }
-    
-    public static void deleteApp(){
-        String name;
-                
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        try{
-            System.out.println("Enter Program name");
-            name = br.readLine();
-            
-            String appName = name+".desktop";
-            File file = new File(getHome() + "/" + appName);
-            
-            if(file.exists()){
-                System.out.println("Do you really want to delete " + appName + "?");
-                System.out.println("Y/n?");
-            
-                String line = br.readLine();
-            
-                if('y' == line.charAt(0) || 'Y' == line.charAt(0)){
-                    file.delete();
-                    System.out.println("File deleted");
-                } 
-        }
-            else{
-                System.out.println(appName + " doesn't exists");
-            }
-        
-        }
-        catch(IOException e){
-            System.out.println(e);
-        }
-        
-        
-    }
-    
-    public static void addApp(){
-        String name, type, exec;
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        try{
-            System.out.println("Enter application name");
-            name = br.readLine();
-            System.out.println("Enter application type");
-            type = br.readLine();
-            System.out.println("Enter exec");
-            exec = br.readLine();
-            
-            String s = "[Desktop Entry]\n" +
-                "Name=" + name + "\n" +
-                "Type=" + type + "\n" +
-                "Exec=" + exec + "\n";
-            
-            Path path = Paths.get(getAppPath(name));
-            File file = new File(getAppPath(name));
-            
-            file.createNewFile();
-            FileWriter fw = new FileWriter(file);
-            fw.write(s);
-            fw.close();
-            System.out.println(name+".desktop created");
-        }
-        catch(IOException e){
-            System.out.println(e);
-        }
+        }*/
     }
 }
---
+++ src/autostart_manager/Menu.java
@@ -0,0 +1,21 @@
+package autostart_manager;
+
+import java.io.BufferedReader;
+
+public class Menu {
+    String cmd;
+    Menu(BufferedReader bufferedReader){
+        while (true){
+            System.out.println("Menu running");
+        }
+    }
+
+    public void addItem(){
+
+    }
+
+    public void help(){
+        System.out.println("HELP opopopopo");
+    }
+
+}
